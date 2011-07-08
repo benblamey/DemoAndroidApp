d157832365bae16c73101ebf18eeb755bd6e2896
Ben Blamey <ben@ml4d.com>
2011-07-08 15:44:20 +0100

    Created project. Created dummy sign-in page that does nothing.

---
+++ src/com/ml4d/ohow/sign_in.java
@@ -0,0 +1,14 @@
+package com.ml4d.ohow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import ml4d.ohow.*;
+
+public class sign_in extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
